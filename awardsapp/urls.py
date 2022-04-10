286d66b1fa6953a2a1d01b790bda54f7706fea1c
kennjr <ken.juniour@student.moringaschool.com>
2022-04-10 04:39:03 +0300

    Added the path for the profile page to the urlpatterns list

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login/', views.login_page, name='login_page'),
     path('register/', views.register_page, name='register_page'),
     path('logout/', views.logout_method, name='logout_page'),
+    path('users/<int:uid>', views.profile_page, name="profile_page")
 ]
 
