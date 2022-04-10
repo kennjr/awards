ff7da3fee1e832330f1d7d4ac09c83ea2c80c8f9
kennjr <ken.juniour@student.moringaschool.com>
2022-04-10 03:28:13 +0300

    Added paths for login, register and logout to the urlpatterns list

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('login/', views.login_page, name='login_page'),
+    path('register/', views.register_page, name='register_page'),
+    path('logout/', views.logout_method, name='logout_page'),
 ]
 
