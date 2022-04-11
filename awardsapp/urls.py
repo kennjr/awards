70956abb73269aa404c0181bfab11607c63ef344
kennjr <ken.juniour@student.moringaschool.com>
2022-04-11 14:42:02 +0300

    Added the urlpattern for the view_project_page.html

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('register/', views.register_page, name='register_page'),
     path('logout/', views.logout_method, name='logout_page'),
     path('users/<int:uid>', views.profile_page, name="profile_page"),
+    path('new-project/', views.new_project, name="new_project_page"),
     path('projects/<int:pid>/new-review', views.review_page, name="review_page"),
+    path('projects/<int:pid>/', views.view_project_page, name="view_project_page")
 ]
 
